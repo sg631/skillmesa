9c4d9cc7fabbea853b76c36b3ef1440d9db8b352
samvithlabs <sb.samvithbha@gmail.com>
2025-10-22 21:17:57 -0700

    Hopefully fixed ListingComponent

--- src/components/ListingElement.jsx
+++ src/components/ListingElement.jsx
@@ -1,25 +1,20 @@
 import React, { useState, useEffect } from "react";
-import { useLocation } from "react-router-dom";
-
-import { TabBarElement, TabContainerElement } from './TabElements.jsx';
 
 // import {  } from "firebase/auth";
 // import { auth } from "../firebase.js";
-
-
 
 function ListingComponent({ width, height, owner, canEdit, type, title, description, tags = [] }) {
     return (
         <>
             <div class="listing">
-                <Image class="listing-thumbnail"></Image>
+                <image class="listing-thumbnail"></image>
                 <span class="listing-title">{title}</span>
                 <p class="listing-description">
                     {description}
                 </p>
                 <div class="controls">
                     <div class="accdisplay">
-                        <Image class="accdisplay-profilepic"></Image>
+                        <image class="accdisplay-profilepic"></image>
                         <span class="accdisplay-text">{owner}</span>
                     </div>
                 </div>
@@ -28,4 +23,4 @@ function ListingComponent({ width, height, owner, canEdit, type, title, descript
     );
 }
 
-export default SignonComponent;
+export default ListingComponent;
